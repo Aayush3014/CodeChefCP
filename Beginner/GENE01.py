be49199387f68cadded98a998430a3321d9ec335
Ayush <83333424+Aayush3014@users.noreply.github.com>
2022-04-03 18:23:18 +0530

    commit

---
+++ Beginner/GENE01.py
@@ -0,0 +1,10 @@
+
+color1,color2 = input().split()
+if color1 == "R" and color2== "B":
+    print("R")
+elif color1 == "R" and color2== "G":
+    print("R")
+elif color1 == "G" and color2== "B":
+    print("B")
+else:
+    print(color1)
