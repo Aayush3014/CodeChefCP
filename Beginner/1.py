a785e9743fa189aa39c270bcf7c52403afbb3206
Ayush <83333424+Aayush3014@users.noreply.github.com>
2022-04-06 21:54:12 +0530

    commit

---
+++ Beginner/1.py
@@ -0,0 +1,20 @@
+# incomplete
+
+# cook your dish here
+for i in range(int(input())):
+    str_len = int(input())
+    s = input()
+    s_list = list(s)
+    count = 0
+    
+    for i in range(0,len(s_list),1):
+        
+        if s_list[i] == "1":
+
+            s_list[i] == "0"
+        for i in range(str_len):
+            target 
+            break
+        count+=1
+        
+    print(count)
